05e88dee4efa4363d6bd519320eebe9293d799a8
iagolaguna <iagolaguna.83@gmail.com>
2020-09-07 19:06:40 -0300

    change style

--- client/src/pages/Home/components/CardBusiness/components/Label/index.tsx
+++ client/src/pages/Home/components/CardBusiness/components/Label/index.tsx
@@ -11,9 +11,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: "flex",
     flexDirection: "column",
   },
-  label: {
-    fontWeight: 350,
-  },
   value: {
     fontWeight: 500,
     paddingLeft: theme.spacing(1),
@@ -24,7 +21,7 @@ const Label = ({ label, value }: LabelProps) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Typography variant="body2" color="primary" className={classes.label}>
+      <Typography variant="body2" color="primary" >
         {label}
       </Typography>
       <Typography variant="body2" gutterBottom className={classes.value}>
